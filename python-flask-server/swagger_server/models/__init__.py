e5dbe1178be6ef7a9ef812c701e84e8e2a7e0f16
RishabhjainCN <rishabh.jain@codenation.co.in>
2019-05-26 00:49:50 +0530

    4th commit

--- python-flask-server/swagger_server/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-# flake8: noqa
-from __future__ import absolute_import
-# import models into model package
-from swagger_server.models.assignmentor import Assignmentor
-from swagger_server.models.assignproj import Assignproj
-from swagger_server.models.proj import Proj
-from swagger_server.models.user import User
